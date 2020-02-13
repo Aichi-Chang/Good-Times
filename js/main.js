9dceffc716fdf13d4a540d53160e1c4fc77c36e2
Aichi Chang <phoebexxxc@gmail.com>
2020-02-13 13:09:55 +0000

    add library in-view

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-import { inView } from 'in-view'
+
 
 // grab header and desktop header
 // insert our content into our desktop header
 function main() {
 
+  var inView = document.createElement('inView')
+  inView.src = 'js/in-view.js'
+  document.head.appendChild(inView)
+
   const header = document.querySelector('.header')
-  const desktopHeader = document.querySelector('header-desktop')
+  const desktopHeader = document.querySelector('.header-desktop')
   
   desktopHeader.innerHTML = header.innerHTML
+
+
   
   // when header enters the viewport, hide the desktop header(remove the visible class)
   // when the header leaves, show it(by add visible class)
